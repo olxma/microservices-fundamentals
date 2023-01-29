bc5af4cb7710655608cedf898bc0c9078c394897
olegmashkov <olegmashkov21@gmail.com>
2023-01-29 23:34:46 +0300

    Implement Resource Rest Controller - Add a few smoke tests for it

---
+++ resource-service/src/main/java/com/epam/microservices/resourseservice/controller/ResourceController.java
@@ -0,0 +1,50 @@
+package com.epam.microservices.resourseservice.controller;
+
+import com.epam.microservices.resourseservice.model.CreationResult;
+import com.epam.microservices.resourseservice.model.DeletionParam;
+import com.epam.microservices.resourseservice.model.DeletionResult;
+import com.epam.microservices.resourseservice.service.ResourceService;
+import com.epam.microservices.resourseservice.validator.MP3TypeConstraint;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/resources")
+public class ResourceController {
+
+    private final ResourceService service;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CreationResult> createResource(@MP3TypeConstraint MultipartFile data) {
+        Integer id = service.createResource(data);
+        return ResponseEntity.ok(new CreationResult(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ByteArrayResource> getResourceById(@PathVariable Integer id) {
+        byte[] data = service.getResourceById(id);
+        return ResponseEntity.ok().body(new ByteArrayResource(data));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<DeletionResult> deleteResources(@Valid @RequestParam("id") DeletionParam param) {
+        List<Integer> deletedIds = service.delete(param.getIds());
+        return ResponseEntity.ok().body(new DeletionResult(deletedIds));
+    }
+}
---
+++ resource-service/src/test/java/com/epam/microservices/resourseservice/controller/ResourceControllerTest.java
@@ -0,0 +1,71 @@
+package com.epam.microservices.resourseservice.controller;
+
+import com.epam.microservices.resourseservice.service.ResourceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ResourceControllerTest {
+
+    private static final byte[] TEST_CONTENT = {1, 2, 3, 4};
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ResourceService service;
+
+    @Test
+    void givenMultipartFile_whenPostWithRequestPart_thenReturnsOK() throws Exception {
+        MockMultipartFile data = new MockMultipartFile("data", "song.mp3", "audio/mpeg", TEST_CONTENT);
+        when(service.createResource(data)).thenReturn(1);
+
+        mockMvc.perform(multipart("/resources").file(data))
+                .andExpect(content().json("{\"id\":1}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenResourceId_whenGetWithId_thenReturnsOK() throws Exception {
+        when(service.getResourceById(1)).thenReturn(new byte[]{1, 2, 3, 4});
+
+        mockMvc.perform(get("/resources/1")
+                        .header(HttpHeaders.RANGE, "bytes=0-1"))
+                .andExpect(content().bytes(new byte[]{1, 2}))
+                .andExpect(status().isPartialContent());
+    }
+
+    @Test
+    void givenResourceIds_whenDelete_thenReturnOK() throws Exception {
+        List<Integer> ids = List.of(1, 2, 3);
+        when(service.delete(ids)).thenReturn(ids);
+
+        mockMvc.perform(delete("/resources").param("id", "1,2,3"))
+                .andExpect(content().json("{\"ids\":[1,2,3]}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenResourcesIdsMoreThan200Chars_whenDelete_thenReturnError() throws Exception {
+        String tooManyIds = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72";
+
+        mockMvc.perform(delete("/resources").param("id", tooManyIds))
+                .andExpect(status().isBadRequest());
+    }
+}
