b53121d7d3bbca18dd9f015d2384cf2e93ba8223
olegmashkov <olegmashkov21@gmail.com>
2023-01-30 19:37:53 +0300

    Add AWS Java SDK - Add dependency - Add properties - Implement AWSConfig

---
+++ resource-service/src/main/java/com/epam/microservices/resourseservice/config/AWSConfig.java
@@ -0,0 +1,31 @@
+package com.epam.microservices.resourseservice.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class AWSConfig {
+
+    @Value("${amazon.s3.endpoint}")
+    private String endpoint;
+
+    @Value("${amazon.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withPathStyleAccessEnabled(true)
+                .build();
+    }
+}
