adb88bdcff30b5e908d510b461ef93bde44e318c
olegmashkov <olegmashkov21@gmail.com>
2023-01-30 15:15:50 +0300

    Create Song Service

---
+++ song-service/src/main/java/com/epam/microservices/songservice/SongServiceApplication.java
@@ -0,0 +1,11 @@
+package com.epam.microservices.songservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SongServiceApplication.class, args);
+    }
+}
