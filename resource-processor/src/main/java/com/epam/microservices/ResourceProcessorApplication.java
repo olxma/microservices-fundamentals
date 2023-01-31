d189baffd156ebe5bef7d6dbe60c42dd166d52dd
olegmashkov <olegmashkov21@gmail.com>
2023-01-31 13:40:04 +0300

    Create Resource Processor as a new module

---
+++ resource-processor/src/main/java/com/epam/microservices/ResourceProcessorApplication.java
@@ -0,0 +1,20 @@
+package com.epam.microservices;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ResourceProcessorApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceProcessorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("Hello world!");
+    }
+}
