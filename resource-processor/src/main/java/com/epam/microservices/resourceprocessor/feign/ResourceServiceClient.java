8cbcf302a0d3b52ca11b2646868f1b36c76d5150
olegmashkov <olegmashkov21@gmail.com>
2023-03-31 12:52:31 +0300

    Module 5 - Service registry - API Gateway

--- resource-processor/src/main/java/com/epam/microservices/resourceprocessor/feign/ResourceServiceClient.java
+++ resource-processor/src/main/java/com/epam/microservices/resourceprocessor/feign/ResourceServiceClient.java
@@ -1,11 +1,13 @@
 package com.epam.microservices.resourceprocessor.feign;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "resourceService")
+@FeignClient(value = "RESOURCE")
+@LoadBalancerClient
 public interface ResourceServiceClient {
 
     @GetMapping("/resources/{id}")
--- resource-processor/src/main/java/com/epam/microservices/resourceprocessor/feign/SongServiceClient.java
+++ resource-processor/src/main/java/com/epam/microservices/resourceprocessor/feign/SongServiceClient.java
@@ -1,11 +1,13 @@
 package com.epam.microservices.resourceprocessor.feign;
 
 import com.epam.microservices.resourceprocessor.model.AudioFileMetadata;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "songService")
+@FeignClient(value = "SONG")
+@LoadBalancerClient
 public interface SongServiceClient {
 
     @PostMapping("/songs")
