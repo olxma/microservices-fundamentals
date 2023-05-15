0166812a9827e168875697debdcf7aba2b53acd5
Oleg Mashkov <38487490+olxma@users.noreply.github.com>
2023-05-15 10:49:11 +0300

    Module 8 (#8)

---
+++ storage-service/src/main/java/com/epam/microservices/storageservice/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.epam.microservices.storageservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+    private static final String ENDPOINT = "/storages";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, ENDPOINT).permitAll()
+                .requestMatchers(HttpMethod.POST, ENDPOINT).hasAnyRole(ADMIN)
+                .requestMatchers(HttpMethod.DELETE, ENDPOINT).hasAnyRole(ADMIN)
+                .anyRequest().authenticated();
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+}
